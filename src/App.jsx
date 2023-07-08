095ba307d7fc52df05966609c471107e529889ea
Isaac Berrio <berriisaax18@hotmail.com>
2023-07-07 19:31:14 -0500

    Ya cree parte del juego, tengo listo el cambio de turnos y la visualisacion de las X y O en el tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const TURNS = {
+  X: "X",
+  O: "O"
+};
+
+const Square = ({ children, active, updateBoard, posicion }) => {
+
+  const className = `square ${active ? "is-selected" : ""}`
+
+  const handleClick = () => {
+    updateBoard(posicion)
+  }
+
+  return <div onClick={handleClick} className={className}>{children}</div>;
+};
 
 function App() {
-  
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turno, setTurno] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turno
+    console.log(newBoard);
+    setBoard(newBoard)
+    const newTurn = turno === TURNS.X ? TURNS.O : TURNS.X
+    setTurno(newTurn)
+  };
+
   return (
-    <>
-      <h1>Hola mundo
-      </h1>
-    </>
-  )
+    <main className="board">
+      <h1>Hola mundo</h1>
+      <div className="game">
+        {board.map((_, index) => {
+          return <Square updateBoard={updateBoard} posicion={index} key={index}>{_}</Square>;
+        })}
+      </div>
+      <section className="turn">
+      <Square active={turno === "X"}>{TURNS.X}</Square>
+      <Square active={turno === "O"}>{TURNS.O}</Square>
+      </section>
+      
+    </main>
+  );
 }
 
-export default App
+export default App;
